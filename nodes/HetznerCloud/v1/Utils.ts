5fab25f22a8cc01cf934523bd1007da9d47ce46b
93094049+kraemr@users.noreply.github.com <93094049+kraemr@users.noreply.github.com>
2024-06-04 14:42:33 +0200

    updated comments

--- nodes/HetznerCloud/v1/Utils.ts
+++ nodes/HetznerCloud/v1/Utils.ts
@@ -20,18 +20,24 @@ export function createDynamicObject(objects: { key: string; value: any }[]): {
 }
 
 /* 
-This Function recursively calls itself incrementing page till last_page reached and then returns an Array of all the received data
+- This Function recursively calls itself incrementing page till 
+last_page reached and then returns an Array of all the received data appended
 
-data is the actual data returned by the api from options, the data gets recursively appended
-arraymergekey specifies which key is array that should be appended to data 
+- data is the array returned in list api calls for example.
+This means data would contain all firewalls directly instead of something.firewalls
+returned by the api from options, the data gets recursively appended
+
+- arraymergekey specifies which key is array that should be appended to data 
 	example: when calling firewalls api {"firewalls":[...]} is returned, arraymergekey would be "firewalls"
-credentialsType is just the credentials name in n8
 
-instance can be  ILoadOptions for loadOptions.ts
+- credentialsType is just the credentials name in n8
+
+- instance can be  ILoadOptions for loadOptions.ts
 instance can also be IExecuteFunctions for the normal actions like: list firewall
-*/
 
-//const timeout = 2000; // 2 seconds timeout
+This function needs a sensible timeout in options, because the api might not respond fast enough, which will throw
+ETIMEDOUT
+*/
 export async function helpPaginate(
 	instance: ILoadOptionsFunctions | IExecuteFunctions,
 	credentialsType: string,
